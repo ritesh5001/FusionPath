ca8ac586603d8536ab09fa048941fccd2af85a4f
ritesh5001 <rgiri5001@gmail.com>
2026-01-30 01:08:02 +0530

    feat: enable dangerous email account linking for Google provider in authentication

--- auth.ts
+++ auth.ts
@@ -13,7 +13,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         strategy: "jwt",
     },
     providers: [
-        Google,
+        Google({ allowDangerousEmailAccountLinking: true }),
         Credentials({
             name: "Credentials",
             credentials: {
